f970dba226fe14f392c38e06ee8da3010f8303c9
sin3point14 <manaschaudhary2000@gmail.com>
2020-02-02 08:20:48 +0530

    add player controls

--- Test Fall.gd
+++ Test Fall.gd
@@ -4,10 +4,26 @@ extends KinematicBody2D
 # var a = 2
 # var b = "text"
 
+const UP = Vector2(0, -1)
 var motion = Vector2()
+export var speed = 200
+export var g = 10
+export var jump_force = 400
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 func _physics_process(delta):
-	motion.y = 400
-	move_and_slide(motion)
+	if Input.is_action_pressed('ui_right'):
+		motion.x = speed
+	elif Input.is_action_pressed('ui_left'):
+		motion.x = -speed
+	else:
+		motion.x = 0
+	if is_on_floor():
+		if Input.is_action_just_pressed('ui_up'):
+			motion.y = -jump_force
+	else:
+		motion.y += g
+		
+	move_and_slide(motion, UP)
