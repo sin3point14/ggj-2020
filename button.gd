6a9cac248d0111be57c546875b5b52593eb17ab8
sin3point14 <manaschaudhary2000@gmail.com>
2020-02-02 12:29:20 +0530

    add button shenanigans

--- button.gd
+++ button.gd
@@ -1,5 +1,5 @@
 extends Sprite
-
+var press = load("res://assets/button_press.png") 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -8,8 +8,8 @@ func _ready():
 	$Area2D.connect("body_entered", self, "hi")
 
 func hi(object):
-	if get_tree().get_root().get_node("World").get_node("player").top_solved == true:
-		queue_free()
+	if get_tree().get_root().get_node("World").get_node("player").top_solved == false:
+		texture = press
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
