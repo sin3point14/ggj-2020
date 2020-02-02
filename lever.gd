4ccf5c06b24805fd83b80ca3d7c5ff7b54ab7a03
sin3point14 <manaschaudhary2000@gmail.com>
2020-02-02 12:38:10 +0530

    add lever shenanigans

--- lever.gd
+++ lever.gd
@@ -1,5 +1,5 @@
 extends Sprite
-var press = load("res://assets/button_press.png") 
+var right = load("res://assets/lever_right.png") 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -8,9 +8,9 @@ func _ready():
 	$Area2D.connect("body_entered", self, "hi")
 
 func hi(object):
-	if get_tree().get_root().get_node("World").get_node("player").top_solved == false:
-		texture = press
-		get_tree().get_root().get_node("World").get_node("player").top_solved = true
+	if get_tree().get_root().get_node("World").get_node("player").right_solved == false:
+		texture = right
+		get_tree().get_root().get_node("World").get_node("player").right_solved = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
