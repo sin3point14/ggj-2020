fbfec76e666ae19e0a9389287844df1d019ef802
sin3point14 <manaschaudhary2000@gmail.com>
2020-02-02 10:05:25 +0530

    fix sprite rotation

--- World.gd
+++ World.gd
@@ -8,24 +8,43 @@ extends Node2D
 #func _ready():
 #	pass # Replace with function body.
 var rot = 0
-var is_rotating = false;
+var is_rotating = 0;
 var init_rot
 var rot_speed = rad2deg(0.1)  # 30 deg/sec
 var final_rot
 
+onready var player = get_node("test")
+
 func _process(delta):
-	if Input.is_action_just_pressed("rotate") and is_rotating == false:
-		is_rotating = true 
+	if Input.is_action_just_pressed("rotate") and is_rotating == 0:
+		is_rotating = 1 
 		init_rot = rot
 		final_rot = init_rot+deg2rad(90)
-	if is_rotating == true:
+	if is_rotating == 1:
 		if rot < init_rot + deg2rad(90):
 			var delta_rot = rot_speed * delta
 			rot += delta_rot
 			rotate(delta_rot)
+			player.rotate(-delta_rot)
 		else:
 			rotate(final_rot - rot)
-			is_rotating = false
+			player.rotate(rot - final_rot)			
+			is_rotating = 0
+	if Input.is_action_just_pressed("rotate_anti") and is_rotating == 0:
+		is_rotating = -1 
+		init_rot = rot
+		final_rot = init_rot-deg2rad(90)
+	if is_rotating == -1:
+		if rot > init_rot - deg2rad(90):
+			var delta_rot = -rot_speed * delta
+			rot += delta_rot
+			rotate(delta_rot)
+			player.rotate(-delta_rot)
+			
+		else:
+			rotate(final_rot - rot)
+			player.rotate(rot - final_rot)						
+			is_rotating = 0
 			
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
